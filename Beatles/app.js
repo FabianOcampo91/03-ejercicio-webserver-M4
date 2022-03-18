4e53561d50ad0a9585d0c885edc4a3633595511d
FabianOcampo91 <faby.ocampo91@gmail.com>
2022-03-18 09:48:29 -0300

    ejercicio 03 Webserver M4

---
+++ Beatles/app.js
@@ -0,0 +1,61 @@
+var http = require('http');
+var fs   = require('fs');
+
+
+
+const beatles=[{
+  name: "John Lennon",
+  birthdate: "09/10/1940",
+  profilePic:"https://blogs.correiobraziliense.com.br/trilhasonora/wp-content/uploads/sites/39/2020/10/CBNFOT081020100047-550x549.jpg"
+},
+{
+  name: "Paul McCartney",
+  birthdate: "18/06/1942",
+  profilePic:"http://gazettereview.com/wp-content/uploads/2016/06/paul-mccartney.jpg"
+},
+{
+  name: "George Harrison",
+  birthdate: "25/02/1946",
+  profilePic:"https://canaldosbeatles.files.wordpress.com/2012/02/george-george-harrison-8321345-438-600.jpg"
+},
+{
+  name: "Richard Starkey",
+  birthdate: "07/08/1940",
+  profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
+}
+]
+http
+.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    let html = fs.readFileSync(__dirname + '/index.html');
+    res.end(html);
+  }
+  
+  if (req.url === '/api') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(beatles));
+  }
+  if (req.url.includes('/api/')) {
+    const params = req.url.replace('%20', ' ').replace('/api/', '');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    const beatle = beatles.find((item) => item.name === params)
+    res.end(JSON.stringify(beatle));
+  }
+
+  if (!req.url.includes('/api')) {
+    const params = req.url.replace('%20', ' ')
+    const beatle = beatles.find((item) => item.name === params);
+    if (beatle) {
+      const html = fs.readFileSync(__dirname +'/template.html', 'utf8');
+      res.writeHead(200, { 'Content-Type':'text/html' });
+      html = html.replace('{name}', beatle.name)
+      html = html.replace('{birthDate}', beatle.birthdate)
+      html = html.replace('{perfilePic}', beatle.profilePic)
+      res.end(html)
+    } else {
+      res.end(`<h1>NO existe $params</h1>`)
+    }
+  }
+})
+.listen(1336, 'localhost', () => console.log('server up...'));
